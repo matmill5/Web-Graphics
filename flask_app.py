e32e5bc3398ddeddb37310592a18b178caec48f2
MMill <S01175693@acad.tri-c.edu>
2019-06-14 09:46:29 -0400

    Flask Redirected

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,11 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+def index():
+    return redirect("/static/examples.html")
 
