e4dd8fff9898f5912c66ecd433db1bcff3bb328b
MMill <S01175693@acad.tri-c.edu>
2019-07-07 12:56:46 -0400

    flask update

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,5 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask, redirect, render_template
+
 import get_data
 
 app = Flask(__name__)
@@ -11,18 +9,18 @@ def index():
     return redirect("/static/examples.html")
 
 @app.route("/demo")
-    def demo():
-        return(render_template("demo.html", name = "Demonstratio", n = 12))
+def demo():
+    return(render_template("demo.html", name = "Demonstratio", n = 12))
 
     
 @app.route("/anon")
-    def anon():
-        return(render_template("demo.html", name = "None", n = 0))
+def anon():
+    return(render_template("demo.html", name = "None", n = 0))
 
 @app.route("/map")
-    def get_map():
+def get_map():
     growth = get_data.get_growth_table()
-        return(render_template("map.html", growth=growth))
+    return(render_template("map.html", growth=growth))
 
 
 
