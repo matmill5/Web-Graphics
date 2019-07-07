639cf0b0f54c58740f20ad81a198feeba8ca88e6
MMill <S01175693@acad.tri-c.edu>
2019-07-07 11:32:44 -0400

    lazy commit

--- get_data.py
+++ get_data.py
@@ -1,19 +1,17 @@
-#module get data
+# module get_data
 
 import csv
 
 def get_growth_table():
-
-    with open('state_populations.csv', 'r') as f:
-    reader = csv.DictReader(f)
     growth_table = {}
+    with open('state_populations.csv','r') as f:
+        reader = csv.DictReader(f)
         for row in reader:
             row = dict(row)
-            print(row)
             code = row["Code"]
             y2010 = float(row["2010"].replace(",",""))
             y2018 = float(row["2018"].replace(",",""))
-            growth = (y2018-y2010)
+            growth = y2018 - y2010
             growth_ratio = growth / y2010
             growth_pct = int(growth_ratio * 100 + 0.5)
             growth_table[code] = growth_pct
